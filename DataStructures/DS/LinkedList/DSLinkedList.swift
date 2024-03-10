5e27d3a1074d157917873df00b838de1e827bad3
Mohit Kumar Singh <mohit.kumar@nickelfox.com>
2024-03-11 00:43:28 +0530

    feat(linkedlist): add solution for question 21

--- DataStructures/DS/LinkedList/DSLinkedList.swift
+++ DataStructures/DS/LinkedList/DSLinkedList.swift
@@ -17,7 +17,59 @@ extension DSLinkedListProtocol {
     }
 }
 
+
+//  Definition for singly-linked list.
+  public class ListNode {
+      public var val: Int
+      public var next: ListNode?
+      public init() { self.val = 0; self.next = nil; }
+      public init(_ val: Int) { self.val = val; self.next = nil; }
+      public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+  }
+ 
+
 class DSLinkedList {
     
-//    static func 
+    // MARK: 21: Merge Two Sorted Lists
+    ///You are given the heads of two sorted linked lists list1 and list2.
+    ///
+    /// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+    ///
+    /// Return the head of the merged linked list.
+    ///
+    /// Time Complexity: O(n)
+    /// Space Complexity: O(1)
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        if list1 == nil && list2 == nil { return nil }
+        
+        var l1 = list1
+        var l2 = list2
+        
+        var result: ListNode? = ListNode(-1)
+        var current = result
+        
+        while l1 != nil && l2 != nil {
+            let val1 = l1!.val
+            let val2 = l2!.val
+            
+            if val1 <= val2 {
+                current?.next = l1
+                l1 = l1?.next
+            } else {
+                current?.next = l2
+                l2 = l2?.next
+            }
+            current = current?.next
+        }
+        
+        if l1 != nil {
+            current?.next = l1
+        }
+        
+        if l2 != nil {
+            current?.next = l2
+        }
+        
+        return result?.next
+    }
 }
