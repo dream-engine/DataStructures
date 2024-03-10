3d392e45a598fee19fc8f60a00246b70458f4eba
Mohit Kumar Singh <mohit.kumar@nickelfox.com>
2024-03-10 23:15:18 +0530

    feat(array): add solution for Question 88

--- DataStructures/DS/Array/Array.swift
+++ DataStructures/DS/Array/Array.swift
@@ -106,7 +106,25 @@ class DSArray: ArrayQuestionsProtocol {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) { // LC88
         var i = m - 1
         var j = n - 1
+        var k = nums1.count - 1
         
+        while i >= 0 && j >= 0 {
+            if nums1[i] > nums2[j] {
+                nums1[k] = nums1[i]
+                i -= 1
+                k -= 1
+            } else {
+                nums1[k] = nums2[j]
+                j -= 1
+                k -= 1
+            }
+        }
+        
+        while j >= 0 { // If elements are left in nums2
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+        }
         
     }
     
