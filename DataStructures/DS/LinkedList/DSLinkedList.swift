a7bdf96797c8a24e231033431ee9af410541be9a
Mohit Kumar Singh <mohit.kumar@nickelfox.com>
2024-03-16 13:02:06 +0530

    feat(linked-list): solve question 2: Add Two Numbers

--- DataStructures/DS/LinkedList/DSLinkedList.swift
+++ DataStructures/DS/LinkedList/DSLinkedList.swift
@@ -211,4 +211,45 @@ class DSLinkedList {
         
         return slow
     }
+    
+    // MARK: 2: Add Two Numbers
+    ///You are given two non-empty linked lists representing two non-negative integers.
+    ///The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    ///
+    ///You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    ///
+    ///Example 1:
+    ///Input: l1 = [2,4,3], l2 = [5,6,4]
+    ///Output: [7,0,8]
+    ///Explanation: 342 + 465 = 807.
+    ///
+    ///Example 2:
+    ///Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    ///Output: [8,9,9,9,0,0,0,1]
+    ///
+    /// Time Complexity: O(n)
+    /// Space Complexity: O(n) creating same number of list nodes
+    func addTwoNumbers2(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var carry = 0
+        
+        var result: ListNode? = ListNode(-1)
+        var current = result
+        var first: ListNode? = l1
+        var second: ListNode? = l2
+        
+        while first != nil || second != nil {
+            let sum = carry + (first?.val ?? 0) + (second?.val ?? 0)
+            current?.next = ListNode(sum % 10)
+            carry = sum / 10
+            current = current?.next
+            first = first?.next
+            second = second?.next
+        }
+        
+        if carry > 0 {
+            current?.next = ListNode(carry)
+        }
+        
+        return result?.next
+    }
 }
