451cdea77ca3f559470f03f0f2a1fc51684603c9
Mohit Kumar Singh <mohit.kumar@nickelfox.com>
2024-03-16 12:47:29 +0530

    feat(linked-list): solve question 876: Middle of the Linked List

--- DataStructures/DS/LinkedList/DSLinkedList.swift
+++ DataStructures/DS/LinkedList/DSLinkedList.swift
@@ -183,4 +183,32 @@ class DSLinkedList {
         
         return reverse
     }
+    
+    // MARK: 876: Middle of the Linked List
+    ///Given the head of a singly linked list, return the middle node of the linked list.
+    
+    ///If there are two middle nodes, return the second middle node.
+    ///Input: head = [1,2,3,4,5]
+    ///Output: [3,4,5]
+    ///Explanation: The middle node of the list is node 3.
+    ///
+    ///Input: head = [1,2,3,4,5,6]
+    ///Output: [4,5,6]
+    ///Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+    ///
+    /// Time Complexity: O(n)
+    /// Space Complexity: O(1)
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        if head == nil { return nil }
+        
+        var slow = head
+        var fast = head
+        
+        while fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        
+        return slow
+    }
 }
